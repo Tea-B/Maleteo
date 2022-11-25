8a8badd27e7d69db806e0059ae348166ef371721
Cheng <chenghaotecno@gmail.com>
2022-11-25 18:02:48 +0100

    Maps

--- src/Pages/SearchPage/Maps/Maps.jsx
+++ src/Pages/SearchPage/Maps/Maps.jsx
@@ -1,21 +1,103 @@
-import React from 'react'
-import { Marker, Popup } from 'leaflet'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import React, { useEffect, useState } from 'react'
+import "./Maps.scss"
+
+import axios from 'axios';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import L from "leaflet";
+
 
 const Maps = () => {
 
+    const [guardians, setGuardians] = useState([]);
+    const [ubications, setUbications] = useState([]);
+
+    useEffect (() => {
+
+        const getData = async ()=> {
+            const { data } = await axios.get("http://localhost:3030/guardians/getall");
+
+            console.log(data);
+            setGuardians(data);
+
+            let mappedUbications = guardiansMap(guardians);
+            console.log(mappedUbications);
+            setUbications(mappedUbications);
+        };
+    
+        getData();
+    
+    }, []);
+
+    let guardiansMap = (guardians) => {
+
+        let mappedUbications = [];
+    
+        guardians.forEach((guardian) => {
+            let { "_id": guardianID, "ubicationsID": ubications } = guardian;
+
+            ubications.forEach((ubication) => {
+                let { _id, name, images, description, disponibility, latitude, longitude } = ubication;
+                mappedUbications.push({guardianID, _id, name, images, description, disponibility, latitude, longitude});
+            });
+        })
+    
+        return mappedUbications;
+    };
+
+    const blueIcon = L.icon({
+        iconUrl: '../../../../assets/images/blue-icon.png',
+        iconSize: [32,32],
+        popupAnchor: [0, -15],
+    });
+
+    const redIcon = L.icon({
+        iconUrl: '../../../../assets/images/red-icon.png',
+        iconSize: [32,32],
+        popupAnchor: [0, -15],
+    });
+
+    const yellowIcon = L.icon({
+        iconUrl: '../../../../assets/images/yellow-icon.png',
+        iconSize: [32,32],
+        popupAnchor: [0, -15],
+    });
+
+    const greenIcon = L.icon({
+        iconUrl: '../../../../assets/images/green-icon.png',
+        iconSize: [32,32],
+        popupAnchor: [0, -15],
+    });
+
+    const purpleIcon = L.icon({
+        iconUrl: '../../../../assets/images/purple-icon.png',
+        iconSize: [32,32],
+        popupAnchor: [0, -15],
+    });
+
+    const icons = [blueIcon, redIcon, yellowIcon, greenIcon, purpleIcon];
+
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <>
+            <div className='map-container'>
+                <MapContainer className='map' center={[40.416761, -3.703691]} zoom={13}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {
+                        ubications.map((ubication) => {
+                            return (
+                                <Marker position={[ubication.latitude, ubication.longitude]} icon={blueIcon}>
+                                    <Popup>
+                                        {ubication.name}
+                                    </Popup>
+                                </Marker>
+                            )
+                        })
+                    }
+                </MapContainer>
+            </div>
+        </>
     )
 }
 
