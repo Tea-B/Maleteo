41400912e7f7035b7b12703e113d1e289fb6955a
AlexProcess <104278094+AlexProcess@users.noreply.github.com>
2022-11-25 12:24:46 +0100

    add generic components, home buttons and searcher, and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
 import { Footer } from './Components/Footer/Footer'
+import { HomeForm } from './Components/HomeForm/HomeForm'
 
 
 export const App = () => {
   return (
     <div>
+      <HomeForm/>
       <Footer/>
     </div>
   )
