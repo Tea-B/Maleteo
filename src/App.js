66df041aa413b67350b49f2e5ad16f59c54c8094
AlexProcess <104278094+AlexProcess@users.noreply.github.com>
2022-11-26 18:45:29 +0100

    fix back icon (header)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 
 import './App.css';
-import { Route, Routes, BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import HomePage from './Pages/HomePage/HomePage';
-import StartPage from './Pages/StartPage/StartPage';
+import { StartPage } from './Pages/StartPage/StartPage';
 import LoginRegisterPage from './Pages/LoginRegisterPage/LoginRegisterPage';
 import SearchPage from './Pages/SearchPage/SearchPage';
 import ProfilePage from './Pages/ProfilePage/ProfilePage';
 import ChatPage from './Pages/ChatPage/ChatPage';
-import { Footer } from './Components/Footer/Footer'
-import { HomeForm } from './Components/HomeForm/HomeForm'
-import { Start1 } from './Components/Start1/Start1';
+import { Start2 } from './Pages/Start2/Start2';
+import { Start1 } from './Pages/Start1/Start1';
+import { Header } from './Components/Header/Header';
 
 
 export const App = () => {
   return (
+    <>
     <Router>
       <Routes>
           <Route exact path="/" element={<StartPage></StartPage>} />
+          <Route exact path="/firststart" element={<Start1></Start1>} />
+          <Route exact path="/secondstart" element={<Start2></Start2>} />
           <Route path="/connect" element={<LoginRegisterPage></LoginRegisterPage>} />
           <Route path="/home" element={<HomePage></HomePage>} />
           <Route path="/map" element={<SearchPage></SearchPage>} />
           <Route path="/profile" element={<ProfilePage></ProfilePage>} />
           <Route path="/chat" element={<ChatPage></ChatPage>} />
       </Routes>
-    </Router>
     <div>
-    <Start1/>
+      <Header></Header>
+      <StartPage/>
     </div>
-  );
+    </Router>
+    
+    </>
+  )};
+
+  export default App;
--- src/Pages/StartPage/StartPage.jsx
+++ src/Pages/StartPage/StartPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './StartPage.scss';
 
-const StartPage = () => {
+export const StartPage = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className='logo__container'>
+        <img className='draw-logo' src='./Assets/Images/maleteo-logo.png' alt=''></img>
+        <img className='b-paragraph__logo' src='./Assets/Images/Maleteo-paragraph-logo.png' alt=''></img>
+      </div>
+    </main>
   )
 }
-
-export default StartPage
