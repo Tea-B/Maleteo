6dae273642f34ea664fecc74d46f7691cd3d68c7
imDorian <dorian.cmd01@gmail.com>
2022-11-27 19:45:12 +0100

    i

--- src/Context/DateOutProvider.jsx
+++ src/Context/DateOutProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react'
 export const contextDateOut = createContext();
 
 const DateOutProvider = ({children}) => {
-    const [dateOut, setDateOut] = useState({})
+    const [dateOut, setDateOut] = useState('0')
   return (
     <contextDateOut.Provider value={{dateOut, setDateOut}}>
         {children}
--- src/Pages/HomePage/ReservePage.jsx
+++ src/Pages/HomePage/ReservePage.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import Footer from '../../Components/Footer/Footer';
 import { contextBaggage } from '../../Context/BaggageProvider';
 import { contextDateIn } from '../../Context/DateInProvider';
 import { contextDateOut } from '../../Context/DateOutProvider';
@@ -10,21 +11,34 @@ const ReservePage = () => {
     const {dateIn} = useContext(contextDateIn)
     const {dateOut} = useContext(contextDateOut)
     const {baggage} = useContext(contextBaggage)
-    console.log(dateIn)
+    
+    
+    
+    const opciones = { month: 'short', day: 'numeric' }
+    // const price24 = 6 * baggage
+
+    const difference = Math.abs(new Date(dateOut) - new Date(dateIn))
+
+    const days = difference/(1000 * 3600 * 24);
+    const price = (6 * baggage * days)    
+
+    
+       
 
   return (
-    <div className='container d-flex flex-column'>
+    <>
+    <div className='reserve-page container d-flex flex-column'>
         <h1>Detalles de tu reserva</h1>
 
-        <div className='details-reserve d-flex flex-row justify-content-around mt-5'>
-            <div className='reserve-box'>
+        <div className='details-reserve d-flex flex-row justify-content-start gap-5 mt-5'>
+            <div className='reserve-box d-flex flex-column justify-content-start '>
                 <h5>Llegada</h5>
-                <p>{dateIn.$D} / {dateIn.$M}</p>
+                <p>{new Date(dateIn).toLocaleDateString('es-SP', opciones)}</p>
             </div>
 
             <div className='reserve-box'>
                 <h5>Recogida</h5>
-                <p>{dateOut.$D} / {dateOut.$M}</p>
+                <p>{new Date(dateOut).toLocaleDateString('es-SP', opciones)}</p>
             </div>
 
             <div className='reserve-box'>
@@ -36,10 +50,10 @@ const ReservePage = () => {
         <div className='price-reserve mt-5'>
 
             <ul className='p-0 d-flex flex-column gap-4 '>
-                <li className='d-flex justify-content-between'><p className='price-text'>Primeras 24 horas 6,00 x 2 equipajes</p><span>10€</span></li>
+                <li className='d-flex justify-content-between'><p className='price-text'>24 horas 6€ x {baggage} equipajes</p><span>{price}€</span></li>
                 <li className='d-flex justify-content-between'><p className='price-text'>Gastos de gestión</p><span>2€</span></li>
                 <li className='d-flex justify-content-between'><p className='price-text'>Servicio asegurado hasta 1000€</p><span>Gratis</span></li>
-                <li className='d-flex justify-content-between'><p className='price-text'>Total</p><span>12€</span></li>
+                <li className='d-flex justify-content-between'><p className='price-text'>Total</p><span>{price + 2 }€</span></li>
             </ul>
 
         </div>
@@ -47,6 +61,8 @@ const ReservePage = () => {
         <button className='btn-reserve'>Reservar</button>
         <Link to={'/home'}><button className='btn btn-secondary'>back home (provisional)</button></Link>
     </div>
+<Footer></Footer>
+    </>
   )
 }
 
