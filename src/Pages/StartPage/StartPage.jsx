235e431c900c0096495c44b4e51105980fe8053a
AlexProcess <104278094+AlexProcess@users.noreply.github.com>
2022-11-28 12:27:50 +0100

    updates from alex branch

--- src/Pages/StartPage/StartPage.jsx
+++ src/Pages/StartPage/StartPage.jsx
@@ -14,15 +14,16 @@ export const StartPage = () => {
   const navigate = useNavigate() 
   return (
     
-   
-    {isLoading} ? <StartModal/> : (<main className='b-main__starts'>
-        <div className='b-card__container'>
+   <>
+    {isLoading ? <StartModal/> : (<main className='b-main__starts'>
+    <div className='b-card__container'>
             <img className='b-hero__image' src='./Assets/Images/start1-image.png' alt=''></img>
             <h3 className='b-starts__paragraph'>Preparáte para liberarte de tu equipaje</h3>
             <p >Encuentra a tu guardián y disfruta a tu manera. Miles de usuarios ya están aprovechando las ventajas</p>
               <button onClick={() => navigate('/secondstart')} className='b-continue__button'>Continuar  👉🏽</button>
         </div>
-    </main>)
+    </main>)}
+    </>
   )
 }
 
